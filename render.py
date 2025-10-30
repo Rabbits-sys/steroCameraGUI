3f28204590659869e4c807e4ca892755b9bbf3ec
lzh <22432100@zju.edu.cn>
2025-10-30 22:45:22 +0800

    Modify rendering function

--- render.py
+++ render.py
@@ -1,9 +1,10 @@
 import json
-import numpy as np
 import os
 import glob
-from PIL import Image
+import logging
 from PyQt5.QtGui import QImage
+
+logger = logging.getLogger(__name__)
 
 def render_temp2img(step_signal, path, height=384, width=512):
     """
@@ -16,24 +17,22 @@ def render_temp2img(step_signal, path, height=384, width=512):
         文件或文件夹路径。
     """
     if os.path.isfile(path):
-        print('单个JSON文件渲染')
+        logger.info("Choose rendering single JSON file")
         dir = os.path.dirname(path)
 
     elif os.path.isdir(path):
-        print(f"{path}目录下所有JSON文件渲染")
+        logger.info(f"Choose rendering all JSON files in the directory {path}")
         dir = path
 
     ir_temp_dict = load_ir_temp(path)
-    sum = len(ir_temp_dict)
-    output_path = ''
+    count = 0
     if ir_temp_dict:
-        count  = 0
         for filename, ir_temp in ir_temp_dict.items():
 
             output_path = dir + '/' + filename + '.jpg'
             save_ir_img(output_path, ir_temp, height, width)
             count += 1
-            progress_value = int(count  / sum * 100)
+            progress_value = int(count  / len(ir_temp_dict) * 100)
 
             progress_info = {
                 'message': f'已处理：{filename}.json',
@@ -42,16 +41,20 @@ def render_temp2img(step_signal, path, height=384, width=512):
                 'progress_value': progress_value,
             }
             step_signal.emit(progress_info)
-            # step_signal.emit(progress_value)
 
         last_image = QImage(output_path)
         result = {
-            'message': f'完成{sum}个温度矩阵的渲染',
-            'sum ': sum,
+            'message': f'完成{count}个温度矩阵的渲染',
+            'count': count,
             'last_image' : last_image
         }
         return result
-    return None
+    else:
+        result = {
+        'message': f'无JSON文件',
+        'count': count,
+        }
+        return result
 
 def load_ir_temp(path):
     """加载温度矩阵
@@ -63,7 +66,7 @@ def load_ir_temp(path):
 
     Returns
     -------
-    dict[str, numpy.ndarray[float]] | None
+    dict[str, list] | None
         字典，键为文件名，值为温度矩阵。若无json文件，则返回None
     """
 
@@ -79,20 +82,17 @@ def load_ir_temp(path):
         json_files = glob.glob(os.path.join(folder_path, "*.json"))
 
     if not json_files:
+        logger.error(f"无JSON文件")
         return None
 
     for file_path in json_files:
         with open(file_path, 'r', encoding='utf-8') as f:
             data = json.load(f)
 
-        # 转换为numpy数组
-        array = np.array(data)
-
         # 获取文件名（不含路径和扩展名）
         filename = os.path.splitext(os.path.basename(file_path))[0]
-        ir_temp_dict[filename] = array
-
-        print(f"成功读取: {filename} -> 形状: {array.shape}")
+        ir_temp_dict[filename] = data
+        logger.info(f"Read file Successfully: {filename}.json")
 
     return ir_temp_dict
 
@@ -103,7 +103,7 @@ def save_ir_img(path, data, height=384 , width=512):
     ----------
     path : str
         文件路径。
-    data : numpy.ndarray[float]
+    data : list
         温度矩阵。
     height : int , default=384
         目标图像的高。
@@ -111,17 +111,42 @@ def save_ir_img(path, data, height=384 , width=512):
         目标图像的宽。
 
     """
-    # reshape为目标形状
-    reshaped_array = data.reshape(height, width)
+    if len(data) != height * width:
+        logger.error(f"Data size mismatch")
 
     # 归一化到0-255范围
-    if reshaped_array.dtype != np.uint8:
-        normalized = (reshaped_array - reshaped_array.min()) / (reshaped_array.max() - reshaped_array.min() + 1e-16)
-        image_data = (normalized * 255).astype(np.uint8)
-    else:
-        image_data = reshaped_array
+    data_normalized = normalize(data)
+
+    # reshape为目标形状
+    array = []
+    for i in range(height):
+        start = i * width
+        end = start + width
+        array.append(data_normalized[start:end])
 
     # 保存图像
-    image = Image.fromarray(image_data)
+    buf = bytes([array[y][x] for y in range(height) for x in range(width) for _ in range(3)])
+    image = QImage(buf, width, height, width * 3, QImage.Format_RGB888).copy()
+
     image.save(path)
-    print(f"成功渲染并保存：{path}")
+    logger.info(f"Render and save successfully: {path}")
+
+def find_min_max(data):
+    """求一维列表的最小值和最大值"""
+    if not data:
+        return None, None
+
+    min_val = max_val = data[0]
+
+    for num in data[1:]:
+        if num < min_val:
+            min_val = num
+        elif num > max_val:
+            max_val = num
+
+    return min_val, max_val
+
+def normalize(data):
+    """列表的广播运算"""
+    min, max = find_min_max(data)
+    return [int((x - min)/(max - min + 1e-16) * 255) for x in data]
